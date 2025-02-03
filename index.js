c10813ee44431e14672f2e27d0d4f86d6c96ac7e
homeira kalim <homeirakalim@gmail.com>
2025-02-03 00:37:11 -0500

    callback functions practice plus first class functions lab complete

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution in this file!
+
+const drivers =['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2)
+
+}
+function returnLastTwoDrivers(drivers0){
+    return drivers.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+//function createFareMultiplier(){
+  //  return multiplyValue;
+//}
